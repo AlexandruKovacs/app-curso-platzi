d57f3a2c28227d43abe82482fdb015bce3d706a6
Alex Kovacs <axkovacs27@gmail.com>
2023-11-18 17:47:38 +0100

    erlaciones user comment y post

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use App\Factory\CategoryFactory;
 use App\Factory\PostFactory;
 use App\Factory\CommentFactory;
-
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,11 +13,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        UserFactory::new()->createMany(10);
         CategoryFactory::new()->createMany(10);
+
         PostFactory::new()->createMany(50, function () {
             return [
                 'comments' => CommentFactory::new()->many(0, 20),
-                'category' => CategoryFactory::random()
+                'category' => CategoryFactory::random(),
+                'user'     => UserFactory::random()
             ];
         });
         CommentFactory::new()->createMany(100);
--- src/Factory/CommentFactory.php
+++ src/Factory/CommentFactory.php
@@ -48,7 +48,8 @@ final class CommentFactory extends ModelFactory
     {
         return [
             'content' => self::faker()->text(),
-            'post' => PostFactory::new(),
+            'post'    => PostFactory::random(),
+            'user'    => UserFactory::random()
         ];
     }
 
